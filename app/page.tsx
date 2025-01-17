7bd40ba31859d1d3dd52564001d95dc8ec24cc89
asherafridi <ashirafridi.work@gmail.com>
2025-01-17 12:11:56 +0500

    2nd deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
+"use client"
 import { Button } from "@/components/ui/button";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(()=>{
+    router.push('/login');
+  },[]);
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <Link href={"/login"}> <Button variant="outline">Login</Button></Link>
+      {/* <Link href={"/login"}> <Button variant="outline">Login</Button></Link> */}
     </div>
   );
 }
